c0f2457693028f4062fa0f9e1755ae432ab76a5b
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-05-19 13:12:31 +0300

    i wrote the function createBtn in the js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const notesContaier = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src = "images/delete.png";
+    notesContaier.appendChild(inputBox).appendChild(img);
+})
