ac34aa090b7df2282e8efb060f63d4172b3e41fa
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-05-19 13:31:34 +0300

    and i worte the in this function (notesContaier) for any time the page is closed my data will be save it

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ const notesContaier = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-createBtn.addEventListener("click",()=>{
+function showNotes(){
+    
+}
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContaier.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable","true");
+    inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
     notesContaier.appendChild(inputBox).appendChild(img);
 })
 
-notesContaier.addEventListener("click",function(e){
-    if(e.target.tagName === "IMG"){
+notesContaier.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
